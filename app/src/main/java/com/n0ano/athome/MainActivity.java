8b5f2f598414ecddf8200311d8fea1eb56fecc96
Donald Dugger <n0ano@n0ano.com>
2018-11-16 08:14:52 -0700

    Keep screen on
    Need to make this an option.

--- app/src/main/java/com/n0ano/athome/MainActivity.java
+++ app/src/main/java/com/n0ano/athome/MainActivity.java
@@ -55,6 +55,7 @@ protected void onCreate(Bundle state)
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
     doit();
 }
