1680c3ba9739d8c029009f017e1de2b284eeecc1
Donald Dugger <n0ano@n0ano.com>
2020-01-05 20:51:59 -0700

    Get meta data from image stream

--- app/src/main/java/com/n0ano/athome/ImageGet.java
+++ app/src/main/java/com/n0ano/athome/ImageGet.java
@@ -10,6 +10,8 @@ import java.io.InputStream;
 import java.net.Authenticator;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 public class ImageGet extends Thread
 {
@@ -43,6 +45,60 @@ public ImageGet(MainActivity act, String server, String type, String user, Strin
     this.img_end = (ImageView)img_end;
 }
 
+private int b2int(byte[] b)
+{
+
+    int i = ((b[3] & 0xFF) << 24) | 
+            ((b[2] & 0xFF) << 16) | 
+            ((b[1] & 0xFF) << 8 ) | 
+            ((b[0] & 0xFF) << 0 );
+    return i;
+}
+
+private void get_meta(InputStream in)
+{
+    String str;
+    char type;
+    byte[] len = new byte[4];
+
+    for (;;) {
+        try {
+            in.read(len, 0, 4);
+            int n = b2int(len);
+            byte[] buf = new byte[n];
+            in.read(buf, 0, n);
+            str = new String(buf, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            Log.d("image stream read error - " + e);
+            return;
+        }
+        type = str.charAt(0);
+        str = str.substring(1);
+        switch (type) {
+
+        case 'E':       // end of meta data
+            Log.d("E:end of metadata");
+            return;
+
+        case 'F':       // file name
+            str = str.substring(0);
+            Log.d("F:" + str);
+            break;
+
+        case 'T':       // title
+            str = str.substring(0);
+            Log.d("F:" + str);
+            break;
+
+        default:        // unknown tag
+            str = str.substring(0);
+            Log.d(type + ":" + str);
+            break;
+
+        }
+    }
+}
+
 public void run()
 {
     String url;
@@ -59,6 +115,7 @@ public void run()
         Log.d("get image from " + url);
         Authenticator.setDefault(new CustomAuthenticator(user, pwd));
 		in_rdr = new URL(url).openStream();
+        get_meta(in_rdr);
         bitmap = BitmapFactory.decodeStream(in_rdr);
         in_rdr.close();
 	} catch (Exception e) {
