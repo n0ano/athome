03f5fe167089e3e88da2be642b27eae34d810607
Donald Dugger <n0ano@n0ano.com>
2020-01-17 21:53:26 -0700

    Overload the log routines to allow an explicit tag

--- app/src/main/java/com/n0ano/athome/Log.java
+++ app/src/main/java/com/n0ano/athome/Log.java
@@ -83,25 +83,16 @@ static void s(String string, MainActivity act)
     act.stream_log(string);
 }
 
-public static void i(String string)
-{
-	if (LOG) android.util.Log.i(TAG, string);
-}
-public static void e(String string)
-{
-	if (LOG) android.util.Log.e(TAG, string);
-}
-public static void d(String string)
-{
-	if (LOG) android.util.Log.d(TAG, string);
-}
-public static void v(String string)
-{
-	if (LOG) android.util.Log.v(TAG, string);
-}
-public static void w(String string)
-{
-	if (LOG) android.util.Log.w(TAG, string);
-}
+public static void i(String string) { i(TAG, string); }
+public static void e(String string) { e(TAG, string); }
+public static void d(String string) { d(TAG, string); }
+public static void v(String string) { v(TAG, string); }
+public static void w(String string) { w(TAG, string); }
+
+public static void i(String tag, String string) { if (LOG) android.util.Log.i(tag, string); }
+public static void e(String tag, String string) { if (LOG) android.util.Log.e(tag, string); }
+public static void d(String tag, String string) { if (LOG) android.util.Log.d(tag, string); }
+public static void v(String tag, String string) { if (LOG) android.util.Log.v(tag, string); }
+public static void w(String tag, String string) { if (LOG) android.util.Log.w(tag, string); }
 
 }
